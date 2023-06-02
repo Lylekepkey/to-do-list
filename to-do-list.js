97b888c7c2410c8b5e3b46f23184591ad58a802b
HOPELESSWONDER <104387577+HOPELESSWONDER@users.noreply.github.com>
2023-06-02 22:49:40 +0200

    so to say done

--- to-do-list.js
+++ to-do-list.js
@@ -1,19 +1,49 @@
-const todoInput = document.getElementById('to-do-list')
-const addInput = document.getElementById('add')
-const listAdd = document.getElementById('to-do-list-1')
+const todoInput = document.getElementById('to-do-input');
+const addButton = document.getElementById('add-button');
+const todoList = document.getElementById('list');
 
 
-addInput.addEventListener('click', function() {
-    const todo = todoInput.value;
+addButton.addEventListener('click', function() {
+    const todo = todoInput.value.trim();
 
-    // Create new row
-    const newRow = document.createElement('ul')
+    if (todo !== '') {
+        
+        const newListItem = document.createElement('li');
+        newListItem.textContent = todo;
 
-    // create new list
-    const newDo = document.createElement('li')
-    todoInfo.textContent = todoInput
-    newRow.appendChild(newDo)
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('delete-button');
 
-    listAdd.appendChild(newRow)
+        const tickButton = document.createElement('button');
+        tickButton.textContent = 'Tick Off';
+        tickButton.classList.add('tick-button');
 
-})
+        
+        newListItem.appendChild(deleteButton);
+        newListItem.appendChild(tickButton);
+
+        
+        todoList.appendChild(newListItem);
+
+        
+        todoInput.value = '';
+    }
+});
+
+
+todoList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-button')) {
+        const listItem = event.target.parentElement;
+        todoList.removeChild(listItem);
+    }
+});
+
+
+todoList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('tick-button')) {
+        const listItem = event.target.parentElement;
+        listItem.classList.toggle('completed');
+    }
+});
